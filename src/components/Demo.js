660e36b6b333772799a5abc6ac96a661d5850506
IanCode09 <ianlombu@protonmail.com>
2020-11-17 02:09:39 +0700

    Demo Components

---
+++ src/components/Demo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Demo = () => {
+    return (
+        <div className='demo_container text-center'>
+            <h3 className='pt-md-5'>Demo Ajaruji</h3>
+
+            <h6 className='mt-5'>Uji coba kemampuan kamu dalam menjawab soal Try Out di Ajar Uji</h6>
+
+            <Button style={{ marginTop: '50px', marginBottom: '80px' }}>Mulai Uji Coba</Button>
+        </div>
+    )
+}
+
+export default Demo
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Feature from '../components/Feature'
+import Demo from '../components/Demo'
 
 const HomeScreen = () => {
     return (
@@ -11,6 +12,7 @@ const HomeScreen = () => {
             <main>
                 <Hero />
                 <Feature />
+                <Demo />
             </main>
         </div>
     )
