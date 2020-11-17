638c7770454893ce7294ef1474fd4fcda178398d
IanCode09 <ianlombu@protonmail.com>
2020-11-17 23:18:08 +0700

    Demo Fixed

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import ButtonCom from './Button'
 
 const Demo = () => {
     return (
         <div className='demo_container text-center'>
-            <h3 className='pt-md-5'>Demo Ajaruji</h3>
+            <h3>Demo Ajar Uji</h3>
 
-            <h6 className='mt-5'>Uji coba kemampuan kamu dalam menjawab soal Try Out di Ajar Uji</h6>
+            <h6>Uji coba kemampuan kamu dalam menjawab soal Try Out di Ajar Uji</h6>
 
-            <Button style={{ marginTop: '50px', marginBottom: '80px' }}>Mulai Uji Coba</Button>
+            <ButtonCom>Mulai Uji Coba</ButtonCom>
+
+            {/* <Button style={{ marginTop: '50px', marginBottom: '80px' }}>Mulai Uji Coba</Button> */}
         </div>
     )
 }
