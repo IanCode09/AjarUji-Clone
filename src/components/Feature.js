bd1baf9d5dfb6f018a386c1e4ee59ed162cd82a1
IanCode09 <ianlombu@protonmail.com>
2020-11-17 23:12:15 +0700

    Feature Fix

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -6,22 +6,20 @@ const Feature = () => {
     return (
         <div className='feature_container'>
             <Container className='text-center'>
-                <h2>Manfaat yang Kamu Dapatkan</h2>
-                <h5 className='mt-4'>Dapatkan pengalaman belajar terbaik dengan fitur menarik di Ajar uji.</h5>
+                <h3><strong>Manfaat yang Kamu Dapatkan</strong></h3>
+                <h5 className='mb-4'>Dapatkan pengalaman belajar terbaik dengan fitur menarik di Ajar uji.</h5>
 
-                <Row>
-                    {Features.map((feature) => (
-                        <Col>
-                            <Image src={feature.image} />
-                            <h5><strong>{feature.featureName}</strong></h5>
-                            <p>{feature.description}</p>
-                        </Col>
-                    ))}
-                        
-
-
-                </Row>
-
+                <div className='feature_item'>
+                    <Row>
+                        {Features.map((feature) => (
+                            <Col>
+                                <Image src={feature.image} />
+                                <h5 className='mb-4'><strong>{feature.featureName}</strong></h5>
+                                <p>{feature.description}</p>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
             </Container>
         </div>
     )
